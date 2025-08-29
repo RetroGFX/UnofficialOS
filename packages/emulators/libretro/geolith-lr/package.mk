77d87f3e2b8ba6fd752385f0d4b47ae543b83392
RetroGFX <zychcomputers@gmail.com>
2025-08-28 21:37:59 -0400

    add new cores (bsnes-mercury-accuracy/balanced, doublecherrygb, geolith, melonds-ds, play, wasm4) patches, virtual, es

---
+++ packages/emulators/libretro/geolith-lr/package.mk
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="geolith-lr"
+PKG_VERSION="1391cef3d373cd2aaf4f8a0478b9d6f831c7dfac"
+PKG_ARCH="aarch64"
+PKG_LICENSE="BSD"
+PKG_SITE="https://github.com/libretro/geolith-libretro"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Geolith is a highly accurate emulator for the Neo Geo AES and MVS."
+PKG_TOOLCHAIN="make"
+
+make_target() {
+cd libretro
+  case "${DEVICE_NAME}" in
+    RK356*)
+      sed -i '/a53/s//a55/g' Makefile
+      sed -i '/rpi3_64/s//RK3566-BSP/' Makefile
+      make -f ./Makefile platform=RK3566-BSP
+      ;;
+    *)
+      make -f ./Makefile platform=${DEVICE_NAME}
+      ;;
+  esac
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp geolith_libretro.so ${INSTALL}/usr/lib/libretro/
+}
