35ea006e3756aa7d58df6c2e144c46e661bd4b63
RetroGFX <zychcomputers@gmail.com>
2025-09-23 17:23:50 -0400

    Remove unneeded cmake options

--- packages/emulators/libretro/ps2-lr/package.mk
+++ packages/emulators/libretro/ps2-lr/package.mk
@@ -30,23 +30,6 @@ if [ "${DISPLAYSERVER}" = "wl" ]; then
   PKG_DEPENDS_TARGET+=" wayland ${WINDOWMANAGER}"
 fi
 
-KG_CMAKE_OPTS_TARGET=" \
-                        -DSDL2_API=TRUE \
-                        -DDISABLE_PCSX2_WRAPPER=1 \
-                        -DPACKAGE_MODE=FALSE \
-                        -DPCSX2_TARGET_ARCHITECTURES=x86_64 \
-                        -DENABLE_TESTS=OFF \
-                        -DEXTRA_PLUGINS=OFF \
-                        -DQT_BUILD=FALSE \
-                        -DBUILD_SHARED_LIBS=OFF \
-                        -DUSE_SYSTEM_LIBS=OFF \
-                        -DDISABLE_ADVANCE_SIMD=ON \
-                        -DUSE_VTUNE=OFF \
-                        -DUSE_VULKAN=ON \
-			                  -DGTK3_API=FALSE \
-                        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=FALSE \
-                        -DCMAKE_BUILD_TYPE=Release"
-
 pre_configure_target() {
   export LDFLAGS="${LDFLAGS} -laio"
 }
